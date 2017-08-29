511e2dc7dcd0798c706a5a56a98952e3a35e0c4a
Hans Nieser <hans@h-o.nl>
2017-08-29 13:48:24 +0200

    OPPO-2035: Override core getUnusedPath() method

--- app/code/community/Loewenstark/UrlIndexer/Model/Url.php
+++ app/code/community/Loewenstark/UrlIndexer/Model/Url.php
@@ -16,6 +16,75 @@ class Loewenstark_UrlIndexer_Model_Url
 
 	protected $_urlKey = false;
 
+    /**
+     * Get requestPath that was not used yet.
+     *
+     * Will try to get unique path by adding -1 -2 etc. between url_key and optional url_suffix
+     *
+     * @param int $storeId
+     * @param string $requestPath
+     * @param string $idPath
+     * @return string
+     */
+    public function getUnusedPath($storeId, $requestPath, $idPath)
+    {
+        if (strpos($idPath, 'product') !== false) {
+            $suffix = $this->getProductUrlSuffix($storeId);
+        } else {
+            $suffix = $this->getCategoryUrlSuffix($storeId);
+        }
+        if (empty($requestPath)) {
+            $requestPath = '-';
+        } elseif ($requestPath == $suffix) {
+            $requestPath = '-' . $suffix;
+        }
+
+        /**
+         * Validate maximum length of request path
+         */
+        if (strlen($requestPath) > self::MAX_REQUEST_PATH_LENGTH + self::ALLOWED_REQUEST_PATH_OVERFLOW) {
+            $requestPath = substr($requestPath, 0, self::MAX_REQUEST_PATH_LENGTH);
+        }
+
+        if (isset($this->_rewrites[$idPath])) {
+            $this->_rewrite = $this->_rewrites[$idPath];
+            if ($this->_rewrites[$idPath]->getRequestPath() == $requestPath) {
+                return $requestPath;
+            }
+        }
+        else {
+            $this->_rewrite = null;
+        }
+
+        $rewrite = $this->getResource()->getRewriteByRequestPath($requestPath, $storeId);
+        if ($rewrite && $rewrite->getId()) {
+            if ($rewrite->getIdPath() == $idPath) {
+                $this->_rewrite = $rewrite;
+                return $requestPath;
+            }
+            // match request_url abcdef1234(-12)(.html) pattern
+            $match = array();
+            $regularExpression = '#^([0-9a-z/-]+?)(-([0-9]+))?('.preg_quote($suffix).')?$#i';
+            if (!preg_match($regularExpression, $requestPath, $match)) {
+                return $this->getUnusedPath($storeId, '-', $idPath);
+            }
+            $match[1] = $match[1] . '-';
+            $match[4] = isset($match[4]) ? $match[4] : '';
+
+            $lastRequestPath = $this->getResource()
+                ->getLastUsedRewriteRequestIncrement($match[1], $match[4], $storeId);
+            if ($lastRequestPath) {
+                $match[3] = $lastRequestPath;
+            }
+            return $match[1]
+                . (isset($match[3]) ? ($match[3]+1) : '1')
+                . $match[4];
+        }
+        else {
+            return $requestPath;
+        }
+    }
+
 	/**
 	 * Get unique product request path
 	 *
